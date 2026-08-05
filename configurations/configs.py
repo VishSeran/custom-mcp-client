f2735caf5095b91b0421612b4a2ce1c070f5f369
VishSeran <vishwa.sovis@gmail.com>
2026-08-05 19:53:43 +0530

    logger updated

--- configurations/configs.py
+++ configurations/configs.py
@@ -5,7 +5,17 @@ def get_logger(name):
     
     try:
         
+        if name is None:
+            raise ValueError("Logger name is missing")
         
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+        )
+               
+        name = name.join("-logger")
+        logger = logging.getLogger(name)
+        return logger
         
     except ValueError as e:
         print(f"Value error: {e}")
