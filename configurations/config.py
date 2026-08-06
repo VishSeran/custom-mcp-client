1161488dbb5b34978d78874976ee3aa472ad5bd4
VishSeran <vishwa.sovis@gmail.com>
2026-08-06 21:08:32 +0530

    get_realtive_path updated

--- configurations/config.py
+++ configurations/config.py
@@ -1,13 +1,23 @@
+from pathlib import Path
+
 from configurations.logger import get_logger
 
 
 MODEL_NAME = "llama-3.3-70b-versatile"
+base_dir = Path.cwd().resolve()
 
 logger = get_logger("config")
 
 def get_realtive_path(path:str):
     
     try:
+        if path is None:
+            raise ValueError("path is missing")
+        
+        file_path = (base_dir/path).resolve()
+        file_path.relative_to(base_dir)
+        
+        return file_path
         
     except ValueError as e:
         logger.error(f"Value Error: {e}")
