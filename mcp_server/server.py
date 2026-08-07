da3af5146f7370ef6b793c2513d34e7fa5e0cb2b
VishSeran <vishwa.sovis@gmail.com>
2026-08-07 11:09:02 +0530

    mcp server init fn updated

---
+++ mcp_server/server.py
@@ -0,0 +1,25 @@
+
+from fastmcp import FastMCP
+
+from configurations.logger import get_logger
+
+
+logger = get_logger("server")
+
+class MCPServer:
+    
+    def __init__(self):
+        
+        try:
+            
+            self.mcp_server = FastMCP(
+                name="FileMCPServer",
+                instructions="""
+                This server facilitates different tools, resources and prompts that support for 
+                file operations."""
+            )
+            logger.info("MCP server has initialized")
+            
+        except Exception:
+            logger.exception("Failed to initialized the MCp server")
+            raise
